fa1974e68582d7ef033ee4dd8344d4fbc63c0691
Ivan Hushchin <oopsgu2006@gmail.com>
2025-04-05 20:22:32 +0300

    backend base created

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('gymdb');
+
+  await app.listen(process.env.PORT || 3001);
+}
+bootstrap();
