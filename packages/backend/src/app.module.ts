4fe01c6ee1d6311e5fca8650bb21910641c8a326
Myroslav <verstyukm@gmail.com>
2025-04-28 20:08:21 +0300

    Update .env path for nest app

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,26 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AccountsModule } from './accounts/accounts.module';
+import { AdminsModule } from './accounts/admins/admins.module';
+import { UsersModule } from './accounts/users/users.module';
+import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { IndexModule } from './index/index.module';
-import { AuthModule } from './auth/auth.module';
-import { AccountsModule } from './accounts/accounts.module';
-import { UsersModule } from './accounts/users/users.module';
-import { AdminsModule } from './accounts/admins/admins.module';
-
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '../../.env',
+      envFilePath: '.env',
     }),
     DatabaseModule.forRoot({
       retryAttempts: 5,
-      delay: 3000
+      delay: 3000,
     }),
-    IndexModule, AuthModule, AccountsModule, UsersModule, AdminsModule
+    IndexModule,
+    AuthModule,
+    AccountsModule,
+    UsersModule,
+    AdminsModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
