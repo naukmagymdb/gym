26ad3d025cfb5a44bd0b857adb8471dbcc80fd29
MaksymSemk <maksumsemen2006@gmail.com>
2025-04-29 23:44:30 +0300

    Added suppliers module to  repository module

--- packages/backend/src/repositories/repository.module.ts
+++ packages/backend/src/repositories/repository.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { DepartmentModule } from './departments/department.module';
 import { ProductsModule } from './products/products.module';
 import { StaffModule } from './staff/staff.module';
+import { SuppliersModule } from './suppliers/suppliers.module';
 import { VisitorModule } from './visitors/visitor.module';
 
 @Module({
-  imports: [DepartmentModule, StaffModule, VisitorModule, ProductsModule],
+  imports: [
+    DepartmentModule,
+    StaffModule,
+    VisitorModule,
+    ProductsModule,
+    SuppliersModule,
+  ],
 })
 export class RepositoryModule {}
