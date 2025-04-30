d9eccc5870b5751dbf2a2cd6001cee9e9c3ed4f5
Ivan Hushchin <oopsgu2006@gmail.com>
2025-04-30 20:52:31 +0300

    added staff trainings endpoint

--- packages/backend/src/repositories/staff/staff.controller.ts
+++ packages/backend/src/repositories/staff/staff.controller.ts
@@ -9,6 +9,9 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { ParseDateStringPipe } from 'src/common/pipes/parse-date-string.pipe';
+import { TrainingResponseDto } from '../trainings/dtos/training-response.dto';
+import { TrainingRepository } from '../trainings/training.repository';
 import { CreateStaffDto } from './dtos/create-staff.dto';
 import { UpdateStaffDto } from './dtos/update-staff.dto';
 import { StaffRepository } from './staff.repository';
@@ -17,7 +20,10 @@ import { StaffRepository } from './staff.repository';
 // @UseGuards(AuthenticatedGuard, RolesGuard)
 @Controller('staff')
 export class StaffController {
-  constructor(private readonly staffRepository: StaffRepository) {}
+  constructor(
+    private readonly staffRepository: StaffRepository,
+    private readonly trainingRepository: TrainingRepository,
+  ) {}
 
   @Get()
   async findAll(
@@ -53,5 +59,24 @@ export class StaffController {
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number) {
     return this.staffRepository.delete(id);
+  }
+
+  @Get(':id/trainings')
+  async getStaffSessions(
+    @Param('id', ParseIntPipe) staff_id: number,
+    @Query('date_of_begin', new ParseDateStringPipe()) date_of_begin?: string,
+    @Query('date_of_end', new ParseDateStringPipe()) date_of_end?: string,
+  ): Promise<TrainingResponseDto[]> {
+    return this.trainingRepository.findAll({
+      queries: {
+        staff_id: staff_id,
+        date_of_begin: date_of_begin,
+        date_of_end: date_of_end,
+      },
+      sortOptions: {
+        sortBy: 'visitor_id',
+        order: 'asc',
+      },
+    });
   }
 }
--- packages/backend/src/repositories/staff/staff.module.ts
+++ packages/backend/src/repositories/staff/staff.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RepositoryService } from '../repository.service';
+import { TrainingModule } from '../trainings/training.module';
 import { StaffController } from './staff.controller';
 import { StaffRepository } from './staff.repository';
 
 @Module({
+  imports: [TrainingModule],
   controllers: [StaffController],
   providers: [StaffRepository, RepositoryService],
 })
