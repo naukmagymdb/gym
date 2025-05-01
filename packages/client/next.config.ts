21d518b6057de969e89c739bb6905bb7d3aa957e
Myroslav <verstyukm@gmail.com>
2025-05-02 01:58:22 +0300

    Fix next config causing weird CSP problems

--- packages/client/next.config.ts
+++ packages/client/next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    async headers() {
-        return [
-            {
-                source: '/',
-                headers: [
-                    {
-                        key: 'Content-Security-Policy',
-                        value: "default-src 'none'; connect-src 'self' http://localhost:3000;",
-                    },
-                ],
-            },
-        ];
-    },
+  /* config options here */
 };
 
 export default nextConfig;
