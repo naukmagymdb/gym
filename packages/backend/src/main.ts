efc9cb9e5b2cf68ba32a227228b3a78c7e7cf068
Myroslav <verstyukm@gmail.com>
2025-05-02 22:43:11 +0300

    Allow patch in CORS on server

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.CLIENT_URL || 'http://localhost:1234',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   });
 
