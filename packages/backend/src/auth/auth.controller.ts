cd8a87d6ac4f0a0e9e8f5a744ac846313c15e6d0
Ivan Hushchin <oopsgu2006@gmail.com>
2025-05-03 04:19:00 +0300

    logout done

--- packages/backend/src/auth/auth.controller.ts
+++ packages/backend/src/auth/auth.controller.ts
@@ -1,17 +1,45 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Request as NestRequest,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { Request as ExpressRequest, Response } from 'express';
 import { LocalAuthGuard } from 'src/auth/guards/LocalAuth.guard';
 
-@UseGuards(LocalAuthGuard)
 @Controller('auth')
 export class AuthController {
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  logIn(@NestRequest() req) {
+    return req.body;
+  }
 
-    @Post('login')
-    async logIn(@Request() req) {
-        return req.body;
-    }
+  @UseGuards(LocalAuthGuard)
+  @Post('admin/login')
+  logInAdmin(@NestRequest() req) {
+    return req.body;
+  }
 
-    @Post('admin/login')
-    async logInAdmin(@Request() req) {
-        return req.body;
-    }
+  @Post('logout')
+  logout(@Req() req: ExpressRequest, @Res() res: Response) {
+    req.logout((err) => {
+      if (err) {
+        return res.status(500).json({ message: 'Logout failed', error: err });
+      }
+
+      req.session.destroy((err) => {
+        if (err) {
+          return res
+            .status(500)
+            .json({ message: 'Session destruction failed', error: err });
+        }
+
+        res.clearCookie('SESSION_ID');
+        return res.status(200).json({ message: 'Logged out successfully' });
+      });
+    });
+  }
 }
