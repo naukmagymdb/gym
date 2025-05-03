3048690ffa6507d8cd2446bb73d04d2aac464f93
Ivan Hushchin <oopsgu2006@gmail.com>
2025-05-03 05:16:01 +0300

    fixed visitor login

--- packages/backend/src/repositories/visitors/visitor.repository.ts
+++ packages/backend/src/repositories/visitors/visitor.repository.ts
@@ -125,6 +125,7 @@ export class VisitorRepository {
         v.phone_num,
         v.email,
         v.age,
+        v.login_password,
         COALESCE(
           json_agg(
             json_build_object(
@@ -143,7 +144,7 @@ export class VisitorRepository {
       LEFT JOIN abonement a ON v.id = a.visitor_id
       LEFT JOIN department d ON a.department_id = d.department_id
       ${whereClause}
-      GROUP BY v.id, v.visitor_name, v.surname, v.patronymic, v.phone_num, v.email, v.age
+      GROUP BY v.id, v.visitor_name, v.surname, v.patronymic, v.phone_num, v.email, v.age, v.login_password
     `;
   }
 }
